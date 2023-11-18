6deecf4410c1c3d1991cf9b923aa00cbba6b8c30
git-nasu <nasu301130@icloud.com>
2023-11-18 23:04:56 +0900

    modified config/*.lua override web-devicons

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,8 +5,41 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+
+--Modes
+--normal_mode 'n'
+--insert_mode 'i'
+--visual_block_mode 'x'
+--term_mode 't'
+--command_mode 'c'
+
+-- Delete a word backward
+keymap.set('n','dw','vb"_d')
+
 -- Exit  insert mode
 keymap.set('i','jj','<Esc>')
 
+-- Split window
+keymap.set('n','ss',':split<Return><C-w>w')
+keymap.set('n','sv',':vsplit<Return><C-w>w')
+
 -- Select all
 keymap.set('n','<C-a>','gg<S-v>G',opts)
+
+-- Move window
+keymap.set('','sh','<C-w>h',opts)
+keymap.set('','sj','<C-w>j',opts)
+keymap.set('','sk','<C-w>k',opts)
+keymap.set('','sl','<C-w>l',opts)
+
+--Visual
+keymap.set('v','<','<gt',opts)
+keymap.set('v','>','>gt',opts)
+
+--Select end of line on visual mode
+keymap.set('v','v','<Esc>',opts)
+
+--Move text up and down
+keymap.set('v','<A-j>',':m .+1<CR>==',opts)
+keymap.set('v','<A-k>',':m .-2<CR>==',opts)
+keymap.set('v','p','"_dp',opts)
---
+++ lua/plugins/web-devicon.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-tree/nvim-web-devicons',
+  lazy = true,
+  opts = {
+    override = {
+      dockerfile = {
+        icon = '',
+        color = '#29B6F6',
+        name =  'Dockerfile'
+      }
+    }
+  }
+}
